2b0d6c05d957a65b56aee1bc4276dabe8b0585e7
Sarya Katby <m.sarya.katby@gmail.com>
2023-08-17 12:38:08 +0300

    adding nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 const App = () => {
   return (
-    <div className="flex items-center justify-center bg-red-400">
-      <h1>hello App</h1>
+    <div className="px-28">
+      <nav className="flex h-[120px] items-center justify-between">
+        <a href="https://github.com/SaryaKatbi">DEVELOPED BY SARYA KATBY</a>
+
+        <ul className="flex gap-x-12">
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/works">Works</a>
+          </li>
+          <li>
+            <a href="/price">Price</a>
+          </li>
+          <li>
+            <a href="/contacts">Contacts</a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      },
+
+      colors: {
+        centerWhite: "rgba(255, 255, 255, 0.3)",
+        middleWhite: "rgba(255, 255, 255, 0)",
+        outWhite: "rgba(255, 255, 255, 0)",
+      },
+    },
   },
   plugins: [],
 };
