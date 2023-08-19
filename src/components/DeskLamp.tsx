4a397a85fb2eb5398c021087dc2a65cb2b494535
Sarya Katby <m.sarya.katby@gmail.com>
2023-08-19 15:10:28 +0300

    Adjusting About page

--- src/components/DeskLamp.tsx
+++ src/components/DeskLamp.tsx
@@ -5,46 +5,44 @@ const DeskLamp = ({ mouseY }: { mouseY: number }) => {
   const redRotate = ((mouseY - 0) * (90 - 70)) / (1000 - 0) + 70;
 
   return (
-    <div className="flex h-fit w-fit flex-col items-center justify-center ">
-      <div className="flex h-fit w-fit origin-bottom flex-col items-center justify-center ">
+    <div>
+      <div className="flex h-fit w-fit origin-bottom flex-col items-center justify-center bg-blue-600">
         <div
-          className={` flex h-fit w-fit origin-bottom flex-col items-center justify-center`}
+          className="origin-bottom-center1 flex h-fit w-fit flex-col items-center justify-center"
           style={{ transform: `rotate(${greenRotation}deg)` }}
         >
           <div
-            className="flex h-fit w-fit origin-bottom flex-col items-center justify-center"
+            className="origin-bottom-center1 flex h-fit w-fit flex-col items-center justify-center "
             style={{ transform: `rotate(${yellowRotate}deg)` }}
           >
             <div
-              className="flex origin-bottom rotate-90 flex-col items-center justify-center"
+              className=" origin-bottom-center1 mb-[2px] flex rotate-90 flex-col items-center justify-center "
               style={{ transform: `rotate(${redRotate}deg)` }}
             >
-              <div className="-mt-[1px] flex h-16 w-32 rotate-180 items-center justify-start rounded-t-full border border-white">
-                <div className="absolute mb-2 ml-6 h-8 w-8 rotate-12 rounded-tl-full border-t-[1px]"></div>
-                <div className="absolute mb-2 ml-6 h-8 w-8 rotate-12 rounded-tl-full border-l-[1px]"></div>
+              <div className="-mb-[1px] h-4 w-8 rounded-t-full bg-white " />
+              <div className="flex h-16 w-32 rotate-180 items-center justify-end rounded-t-full border-[2px] border-white">
+                <div className="absolute -mt-2 mr-6 h-8 w-8 -rotate-12 rounded-tr-full border-t-[1px]" />
+                <div className="absolute -mt-2 mr-6 h-8 w-8 -rotate-12 rounded-tr-full border-r-[1px]" />
               </div>
-              <div className=" -mt-[1px] h-2 w-3 border border-white"></div>
-              <div className="flex h-9 w-9 items-center justify-center rounded-full border">
-                <div className="h-5 w-5 rounded-full border"></div>
+              <div className=" -mt-[2px] h-2 w-3 border-[2px] border-white" />
+              <div className=" -mt-[2px] flex h-9 w-9 items-center justify-center rounded-full border-[2px] border-white">
+                <div className="h-5 w-5 rounded-full border-[2px] border-white" />
               </div>
             </div>
-
-            <div className="h-44 w-[2px] bg-white"></div>
-            <div className="flex h-9 w-9 items-center justify-center rounded-full border">
-              <div className="h-5 w-5 rounded-full border border-white"></div>
+            <div className="-mb-[1px] h-44 w-[2px] overflow-hidden bg-white " />
+            <div className=" mb-[1px] flex h-9 w-9 items-center justify-center rounded-full border-[2px] border-white">
+              <div className="h-5 w-5 rounded-full border-[2px] border-white" />
             </div>
           </div>
-
-          <div className="h-44 w-[2px] bg-white"></div>
-          <div className="flex h-9 w-9 items-center justify-center rounded-full border">
-            <div className="h-5 w-5 rounded-full border"></div>
+          <div className="h-44 w-[2px] overflow-hidden bg-white" />
+          <div className="flex h-9 w-9 items-center justify-center rounded-full border-[2px] border-white">
+            <div className="h-5 w-5 rounded-full border-[2px] border-white" />
           </div>
         </div>
-
-        <div className="h-44 w-[2px] bg-white"></div>
-        <div className="h-4 w-6 rounded-t-full border border-white"></div>
-        <div className="h-4 w-6 border"></div>
-        <div className="h-5 w-32 rounded-t-lg border border-white"></div>
+        <div className="h-44 w-[2px] overflow-hidden bg-white" />
+        <div className="-mb-[2px] h-4 w-6 rounded-t-full border-[2px] border-white" />
+        <div className="-mb-[2px] h-4 w-6 border-[2px] border-white" />
+        <div className="h-5 w-32 rounded-t-lg border-[2px] border-white" />
       </div>
     </div>
   );
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -5,11 +5,38 @@ const About = () => {
   const [mouseY, setMouseY] = useState(0);
   return (
     <section
+      className="flex w-full items-end justify-around"
       onMouseMove={(event) => {
         setMouseY(event.clientY);
       }}
     >
       <DeskLamp mouseY={mouseY} />
+
+      <article className="font-primmariFont flex h-[380px] max-w-2xl flex-col justify-between text-white">
+        <h2 className="text-6xl">About Me</h2>
+        <div className="flex flex-col gap-6">
+          <p className="">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis.
+          </p>
+        </div>
+      </article>
     </section>
   );
 };
