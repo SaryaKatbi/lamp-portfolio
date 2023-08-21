225b88304376ef4ab365948353e3cd0d8d08f784
Sarya Katby <m.sarya.katby@gmail.com>
2023-08-21 15:41:51 +0300

    adjusting About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,18 +2,24 @@ import { useState } from "react";
 import DeskLamp from "../components/DeskLamp";
 
 const About = () => {
+  // const roseRotate = ((mouseY - 0) * (12 - 20)) / (1000 - 0) + 12;
+
+  const shadowPosition = ((mouseX - 0) * (12 - 20)) / (1000 - 0) + 12;
+
   const [mouseY, setMouseY] = useState(0);
+  const [mouseX, setMouseX] = useState(0);
   return (
     <section className="flex items-center justify-center ">
       <div
-        className="absolute -top-8 -z-10 flex w-full items-end justify-center gap-80 px-28"
+        className="absolute bottom-32 -z-10 flex items-end justify-evenly gap-[360px] px-28"
         onMouseMove={(event) => {
           setMouseY(event.clientY);
         }}
       >
         <DeskLamp mouseY={mouseY} />
-        <article className="font-secondaryFont flex h-fit w-[620px] flex-col gap-4 text-start font-light text-white ">
-          <h2 className="text-start text-6xl uppercase ">About Me</h2>
+
+        <article className="font-secondaryFont -mb-16 flex w-[620px] flex-col gap-4 font-light text-white ">
+          <h2 className="text-6xl uppercase">About Me</h2>
           <p className="text-base">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
@@ -33,7 +39,14 @@ const About = () => {
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis.
           </p>
-          <div className="-mb-16 h-32 w-[800px] rounded-full bg-gradient-radial" />
+          <div
+            onMouseMove={(e) => {
+              setMouseX(e.clientX);
+            }}
+            style={{ transform: `translate(${shadowPosition})` }}
+          >
+            <div className="left-0 -ml-28 h-32 w-[800px] -rotate-1 bg-gradient-radial " />
+          </div>
         </article>
       </div>
     </section>
