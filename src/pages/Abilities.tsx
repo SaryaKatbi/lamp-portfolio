8895f4defa738890719d6b19ff50b7cfe0d08ac8
Sarya Katby <m.sarya.katby@gmail.com>
2023-08-24 13:00:53 +0300

    Align the cursor light spot

--- src/pages/Abilities.tsx
+++ src/pages/Abilities.tsx
@@ -3,10 +3,10 @@ import { useState } from "react";
 import Skils from "../components/Skils";
 
 const Abilities = () => {
-  const [mouseX, setMouseX] = useState({});
+  const [mouseX, setMouseX] = useState(0);
   const mouseMoveX = mouseX;
 
-  const [mouseY, setMouseY] = useState({});
+  const [mouseY, setMouseY] = useState(0);
   const mouseMoveY = mouseY;
 
   return (
@@ -17,15 +17,13 @@ const Abilities = () => {
         setMouseY(event.clientY);
       }}
     >
-      <div className="relative block h-full w-full">
-        <div
-          className="bg-gradient-white-radial fixed left-0 top-0 h-96 w-96 rounded-full opacity-50 transition-opacity group-hover:opacity-100"
-          style={{
-            transform: `translate(${mouseMoveX}px, ${mouseMoveY}px)`,
-          }}
-        />
-        <Skils skilsColor="absolute text-[#000000]" />
-      </div>
+      <div
+        className="fixed -left-[10%] -top-[20%] h-96  w-96 self-center rounded-full bg-gradient-white-radial opacity-0 transition-opacity group-hover:opacity-100 "
+        style={{
+          transform: `translate(${mouseMoveX}px, ${mouseMoveY}px)`,
+        }}
+      />
+      <Skils skilsColor="absolute text-[#000000]" />
     </section>
   );
 };
