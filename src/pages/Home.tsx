325060c1ba10c6badf1708f6c2c381c5ae1ad3e1
Sarya Katby <m.sarya.katby@gmail.com>
2023-08-26 14:23:38 +0300

    responsive Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,21 +6,27 @@ const Home = () => {
     <div className="group px-28">
       <div className="flex justify-center">
         <Lamp />
-        <div className="absolute h-96 w-96 -translate-y-3 bg-gradient-radial-light opacity-0 transition-opacity duration-500 group-hover:opacity-100 " />
+        <div className="absolute h-36 w-36 -translate-y-3 bg-gradient-radial-light opacity-0 transition-opacity duration-500 group-hover:opacity-100 lg:h-96 lg:w-96 " />
       </div>
 
       <section className="mt-20 flex h-[700px] flex-col items-center justify-center self-center">
-        <h2 className="mb-7 mt-16 font-secondaryFont text-9xl font-medium">
+        {/* mb-7 mt-16 font-secondaryFont text-9xl font-medium */}
+        <h2
+          className="
+         -mt-9 mb-6 text-center font-secondaryFont
+        text-5xl font-medium lg:mb-7 lg:mt-16 lg:text-9xl
+        "
+        >
           Web Designer
         </h2>
-        <h3 className="mb-24 font-secondaryFont text-5xl font-light">
+        <h3 className="mb-16 text-center font-secondaryFont text-3xl font-light lg:mb-24 lg:text-5xl">
           SARYA KATBY
         </h3>
         <Button />
       </section>
 
       <section className="-mt-28 flex items-center justify-center overflow-hidden">
-        <div className="h-32 w-[700px] rounded-full bg-gradient-radial opacity-0 transition-opacity duration-500 group-hover:opacity-100 " />
+        <div className="h-20 w-full rounded-full bg-gradient-radial opacity-0 transition-opacity duration-500 group-hover:opacity-100 lg:h-32 lg:w-[700px] " />
       </section>
     </div>
   );
