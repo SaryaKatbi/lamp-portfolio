51d8e1466d6c70baaf371f2fcc04e077cb28db34
Sarya Katby <m.sarya.katby@gmail.com>
2023-08-28 16:54:45 +0300

    Res Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,10 @@
 const Button = () => {
   return (
     <a href="/about">
-      <div className="h-fit w-fit cursor-pointer border px-12 py-4 font-secondaryFont text-white transition-all duration-200 hover:bg-white hover:text-primmaryDark">
+      <div
+        className="h-fit w-fit cursor-pointer border px-8 py-4 text-center font-secondaryFont text-sm text-white transition-all duration-200 hover:bg-white hover:text-primmaryDark
+      lg:px-12 lg:py-4 lg:text-base "
+      >
         ABOUT ME
       </div>
     </a>
