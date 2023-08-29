b1bf50db52d3d6eb773ac44e1725be391b46d8d2
Sarya Katby <m.sarya.katby@gmail.com>
2023-08-29 18:10:15 +0300

    Responsive Abilities

---
+++ src/components/ResSkils.tsx
@@ -0,0 +1,162 @@
+const ResSkils = () => {
+  return (
+    <div className="flex h-full w-full flex-col items-center justify-center text-lg">
+      <div className="flex h-full w-full justify-between px-3">
+        <a
+          href="https://developer.mozilla.org/en-US/docs/Web/HTML"
+          target="_blank"
+          className="text-html"
+        >
+          <span>HTML</span>
+        </a>
+        <a
+          href="https://www.javascript.com/"
+          target="_blank"
+          className="text-javaScript"
+        >
+          <span>JavaScript</span>
+        </a>
+      </div>
+
+      <div className="flex h-full w-full justify-between px-3">
+        <a href="https://react.dev/" target="_blank" className="text-react">
+          <span>React</span>
+        </a>
+
+        <a
+          href="https://reactnative.dev/"
+          target="_blank"
+          className="text-reactNative"
+        >
+          <span>React Native</span>
+        </a>
+      </div>
+
+      <div className="flex h-full w-full justify-between px-3">
+        <a
+          className="text-css"
+          href="https://developer.mozilla.org/en-US/docs/Web/CSS"
+          target="_blank"
+        >
+          <span>CSS</span>
+        </a>
+        <a
+          className="text-typeScript"
+          href="https://www.typescriptlang.org/"
+          target="_blank"
+        >
+          <span>TypeScript</span>
+        </a>
+      </div>
+
+      <div className="flex h-full w-full justify-between px-3">
+        <a
+          className="text-illustrator"
+          href="https://www.adobe.com/products/illustrator.html"
+          target="_blank"
+        >
+          <span>Illustrator</span>
+        </a>
+        <a
+          className="text-inDesign"
+          href="https://www.adobe.com/products/indesign.html"
+          target="_blank"
+        >
+          <span>InDesign</span>
+        </a>
+      </div>
+
+      <div className="flex h-full w-full justify-between px-3">
+        <a className="text-nodeJs" href="https://nodejs.org/en" target="_blank">
+          <span>Node.Js</span>
+        </a>
+        <a className="text-figma" href="https://www.figma.com/" target="_blank">
+          <span>Figma</span>
+        </a>
+      </div>
+
+      <div className="flex h-full w-full justify-between px-3">
+        <a
+          className="text-photoshop"
+          href="https://www.adobe.com/products/photoshop.html"
+          target="_blank"
+        >
+          <span>Photoshop</span>
+        </a>
+        <a
+          className="text-lightroom"
+          href="https://www.adobe.com/products/photoshop-lightroom.html"
+          target="_blank"
+        >
+          <span>Lightroom</span>
+        </a>
+      </div>
+
+      <div className="flex h-full w-full justify-between px-3">
+        <a
+          className="text-audition "
+          href="https://www.adobe.com/products/audition.html"
+          target="_blank"
+        >
+          <span>Audition</span>
+        </a>
+        <a
+          className="text-afterEffects"
+          href="https://www.adobe.com/products/aftereffects.html"
+          target="_blank"
+        >
+          <span>After Effects</span>
+        </a>
+      </div>
+
+      <div className="flex h-full w-full justify-between px-3">
+        <a
+          className="text-premierePro"
+          href="https://www.adobe.com/products/premiere/free-trial-download.html"
+          target="_blank"
+        >
+          <span>Premiere Pro</span>
+        </a>
+        <a className="text-github" href="https://github.com" target="_blank">
+          <span>Github</span>
+        </a>
+      </div>
+
+      <div className="flex h-full w-full justify-between px-3">
+        <a
+          className="text-blender"
+          href="https://www.blender.org/"
+          target="_blank"
+        >
+          <span>Blender</span>
+        </a>
+        <a
+          className="text-behance"
+          href="https://www.behance.net/"
+          target="_blank"
+        >
+          <span>Bēhance</span>
+        </a>
+      </div>
+
+      <div className="flex h-full w-full justify-between px-3">
+        <a
+          className="text-dribbble "
+          href="https://dribbble.com/shots"
+          target="_blank"
+        >
+          <span>dribbble</span>
+        </a>
+        <a
+          className="text-vsCode"
+          href="https://code.visualstudio.com/"
+          target="_blank"
+        >
+          <span>VS Code</span>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default ResSkils;
--- src/pages/Abilities.tsx
+++ src/pages/Abilities.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 import Skils from "../components/Skils";
+import ResSkils from "../components/ResSkils";
 
 const Abilities = () => {
   const [mouseX, setMouseX] = useState(0);
@@ -17,9 +18,9 @@ const Abilities = () => {
       }}
       className="group px-5 md:px-10 lg:px-16 xl:px-36 "
     >
-      <div className="relative -z-30 h-full w-full">
+      <div className="relative -z-30 h-full w-full max-lg:hidden">
         <div
-          className="blob fixed aspect-square h-[0] rounded-full bg-gradient-white-radial opacity-0 blur-2xl
+          className="blob fixed aspect-square h-[0] rounded-full bg-gradient-white-radial opacity-100 blur-2xl
           transition-opacity duration-500 group-hover:opacity-100 lg:h-[500px]"
           style={{
             left: `${mouseMoveX}px`,
@@ -27,6 +28,10 @@ const Abilities = () => {
           }}
         />
         <Skils skilsColor="absolute text-[#000000]" />
+      </div>
+
+      <div className="lg:hidden">
+        <ResSkils />
       </div>
     </section>
   );
