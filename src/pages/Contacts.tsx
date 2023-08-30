5cbb2b9b54367aa0a4897d960326e0d861cdea27
Sarya Katby <m.sarya.katby@gmail.com>
2023-08-30 12:45:46 +0300

    Responsive Contacts

--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -12,9 +12,9 @@ const Contacts = () => {
       onMouseMove={(event) => {
         setMouseY(event.clientY);
       }}
-      className="group flex h-[840px] w-full items-center justify-between overflow-hidden pb-40 font-secondaryFont"
+      className="group flex h-[840px] w-full items-center justify-between overflow-hidden font-secondaryFont"
     >
-      <div className="opacity-0 transition-opacity duration-500 group-hover:opacity-70 ">
+      <div className="opacity-0 transition-opacity duration-500 group-hover:opacity-70 max-lg:hidden ">
         <div
           className=" left-0 h-[800px] w-32 bg-gradient-radial"
           style={{
@@ -24,19 +24,21 @@ const Contacts = () => {
         />
       </div>
 
-      <article className="absolute px-28">
+      <article className="absolute px-5 max-lg:left-0 md:px-10 lg:px-16 xl:px-36">
         <div className="flex w-fit flex-col gap-4 font-extralight">
-          <h2 className="text-5xl font-light uppercase">Let's Chat!</h2>
-          <p className="w-[800px] text-2xl leading-relaxed tracking-wide ">
+          <h2 className=" text-2xl font-light uppercase sm:text-4xl lg:text-5xl">
+            Let's Chat!
+          </h2>
+          <p className="text-lg leading-relaxed tracking-wide lg:w-[800px] lg:text-2xl ">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas
             cumque quis facere! Fugiat fuga repudiandae explicabo nihil alias
             eaque doloremque exercitationem, qui a neque quasi numquam minima
             iure odit quidem.
           </p>
 
-          <div className="mt-10 flex justify-between">
-            <div className="flex flex-col gap-y-20">
-              <div>
+          <div className="mt-10 flex flex-col justify-between lg:flex-row">
+            <div className="flex flex-col lg:gap-y-20">
+              <div className="mt-6">
                 <h4 className="text-[#cccccc]">Send me an email</h4>
                 <a
                   className="w-fit border-b-[1px] border-white border-opacity-0 text-2xl transition-all hover:border-opacity-100"
@@ -47,7 +49,7 @@ const Contacts = () => {
                 </a>
               </div>
 
-              <div>
+              <div className="mt-6">
                 <h4 className="text-[#cccccc]">Chat on WhatsApp</h4>
                 <a
                   className="w-fit border-b-[1px] border-white border-opacity-0 text-2xl transition-all hover:border-opacity-100"
@@ -59,8 +61,8 @@ const Contacts = () => {
               </div>
             </div>
 
-            <div className="flex flex-col gap-y-20">
-              <div>
+            <div className="flex flex-col lg:gap-y-20">
+              <div className="mt-6">
                 <h4 className="text-[#cccccc]">Follow me on Twitter</h4>
                 <a
                   className="w-fit border-b-[1px] border-white border-opacity-0 text-2xl transition-all hover:border-opacity-100"
@@ -71,7 +73,7 @@ const Contacts = () => {
                 </a>
               </div>
 
-              <div>
+              <div className="mt-6">
                 <h4 className="text-[#cccccc]">Connect on LinkedIn</h4>
                 <a
                   className="w-fit border-b-[1px] border-white border-opacity-0 text-2xl transition-all hover:border-opacity-100"
@@ -88,7 +90,7 @@ const Contacts = () => {
 
       <WallLamp
         mouseY={mouseY}
-        className="-mr-[100px] -mt-[600px] h-fit w-fit translate-y-10 "
+        className="right-0 -mr-[100px] -mt-[600px] h-fit w-fit translate-y-10 max-lg:hidden  "
       />
     </section>
   );
